b9dd52a3b406c6fbc5979edd2c846895509cc957
Doug Alexander <doug@proximodev.com>
2024-10-10 17:15:59 -0700

    Cleanup functions.php

--- child-theme/inc/block-styles.php
+++ child-theme/inc/block-styles.php
@@ -16,4 +16,27 @@ function register_button_block_styles() {
 }
 
 add_action( 'init', 'register_button_block_styles' );
+
+
+
+
+/*
+* Block: Update cover with mobile image
+*/
+
+// Update the cover block to support second mobile image with picture tag
+add_filter('render_block_core/cover', 'my_responsive_cover_render', 1, 2);
+function my_responsive_cover_render($content, $block) {
+// If has mobile image
+    if (isset($block['attrs']['mobileImageURL'])) {
+        $image = $block['attrs']['mobileImageURL'];
+        $content = preg_replace(
+        '<img class="wp-block-cover__image.+\/>/Ui',
+                      "<picture><source srcset='{$image}' media='(max-width:800px)'>$0</picture>",
+        $content
+        );
+    }
+    return $content;
+}
+
 ?>
